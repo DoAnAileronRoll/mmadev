a01036a176935d2637f0144e81fa69e4e148b007
AileronRoll <gregrjohnson1@gmail.com>
2025-10-10 21:32:48 -0400

    asdf

---
+++ getEventNames.py
@@ -0,0 +1,32 @@
+import wikipedia
+from bs4 import BeautifulSoup
+from utilities import * 
+import csv
+import pandas as pd
+
+
+def getEventNames():
+    result = wikipedia.page("List of UFC events") 
+    html = result.html()
+
+    soup = BeautifulSoup(html, features='html.parser')
+    table = soup.find('table', attrs={'id':'Past_events'})
+    tableBody = table.find('tbody')
+
+    tableRows = tableBody.find_all('tr')
+
+    data = []
+
+    ufc12Found = False
+    i = 1
+    while not ufc12Found :
+        row = tableRows[i]
+        rowCells = row.find_all('td')
+        eventName = rowCells[1].text.strip()
+        data.append(eventName)
+        if(eventName == 'UFC 12: Judgement Day'):
+            ufc12Found = True
+        i+=1
+    df = pd.DataFrame(data)
+    df.Name = 'EventName'
+    df.to_csv('data.csv', index=False, encoding='utf-8', header=['EventName'])
---
+++ utilities.py
@@ -0,0 +1,18 @@
+def parseDecision (text):
+    textList = text.replace('(', '').replace(')','').replace(',','').split(' ')
+    if len(textList) == 2: 
+        textList.extend(['','',''])
+    return textList 
+#TKO (spinning back elbow and punches)	
+def parseFinish (text):
+    return text.replace(')','').split('(')
+
+#TKO (spinning back elbow and punches)	
+def parseNoContest (text):
+    return text.replace(')','').split('(')
+
+def parseWeight (text):
+    if(text.find('lb)') > -1):
+        splitted = text.replace(')','').split('(')
+        return [splitted[0].strip(),splitted[1].strip()]
+    return [text, 0]
